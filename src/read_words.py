7a1341053dc0ab0be7a5c06d0dbaf16a7bbb4eaa
Nirmit Shah <kermitnirmit@gmail.com>
2020-10-06 16:41:02 -0400

    fixed files

--- src/read_words.py
+++ src/read_words.py
@@ -1,5 +1,5 @@
 def load_words():
-    with open('../english-words/words_alpha.txt') as word_file:
+    with open('../words/words_alpha.txt') as word_file:
         valid_words = set(word_file.read().split())
 
     return valid_words
